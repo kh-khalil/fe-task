ca1445c7752db9fd15cfdd2128dc1a04b7fb0061
Khaled Khalil <khaled@sherzal.com>
2021-11-15 09:36:32 +0200

    fe task init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { TeamComponent } from './components/team/team.component';
+import { OurClientsComponent } from './components/our-clients/our-clients.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutUsComponent,
+    PortfolioComponent,
+    TestimonialComponent,
+    TeamComponent,
+    OurClientsComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
