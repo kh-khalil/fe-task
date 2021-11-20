4c998e8a601ef441fd9b8b368b1f713567629a57
Khaled Khalil <khaled@sherzal.com>
2021-11-20 11:16:32 +0200

    nav bar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  public pages = [
+    {
+      name: 'home',
+      active: true
+    },
+    {
+      name: 'about us',
+      active: false
+    },
+    {
+      name: 'portfolio',
+      active: false
+    },
+    {
+      name: 'team',
+      active: false
+    },
+    {
+      name: 'contact us',
+      active: false
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  activateClass(page: any) {
+    this.pages.forEach(element => {
+      element.active = false;
+    });
+    page.active = !page.active;
+  }
 }
